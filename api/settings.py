5ae04dce2ba49093591743915cd22aa12184bf80
Dan Bliss <danbliss14027@gmail.com>
2024-01-08 13:56:05 -0500

    got .env working

--- api/settings.py
+++ api/settings.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     secret_key: str = "Example Key"
     hash_algorithm: str = "HS256"
     token_life_time: int = 60
+
+load_dotenv()
     
 settings = Settings()
