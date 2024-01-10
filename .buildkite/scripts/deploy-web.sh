af11f4e916633a47f68fc291f1b312567dba8ab7
Dan Bliss <danbliss14027@gmail.com>
2024-01-09 21:41:36 -0500

    fix ui deploy

--- .buildkite/scripts/deploy-web.sh
+++ .buildkite/scripts/deploy-web.sh
@@ -11,3 +11,5 @@ fi
 # Create or overwrite the .env file
 echo "REACT_APP_API_URL=${BUILDKITE_ENV_REACT_APP_API_URL}" > .env
 echo "REACT_APP_WS_URL=${BUILDKITE_ENV_REACT_APP_WS_URL}" >> .env
+
+docker compose up --build -d
