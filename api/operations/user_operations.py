fabc9e3c69bccee049a9a24ad481aaacea83bf2f
Dan Bliss <danbliss14027@gmail.com>
2024-01-15 15:53:00 -0500

    readded create user

--- api/operations/user_operations.py
+++ api/operations/user_operations.py
@@ -1,7 +1,8 @@
 from typing import Optional
 from sqlalchemy.orm import Session
 from models.models import AdminUser
-
+from schemas.user_schema import AdminUserCreate
+from operations.authentication import hash_password
 
 # get user by id
 def get_user_by_id(db: Session, user_id: int):
@@ -11,3 +12,21 @@ def get_user_by_id(db: Session, user_id: int):
 # get user by username
 def get_user_by_username(db: Session, username: str) -> Optional[AdminUser]:
     return db.query(AdminUser).filter(AdminUser.username == username).first()
+
+
+# create a user
+def add_user(db: Session, user: AdminUserCreate):
+    password = hash_password(user.unhashed_password)
+    
+    user = AdminUser(
+        first_name = user.first_name,
+        last_name = user.last_name,
+        username = user.username,
+        password = password,
+        super_user = user.super_user,
+    )
+    
+    db.add(user)
+    db.commit()
+    
+    return user
